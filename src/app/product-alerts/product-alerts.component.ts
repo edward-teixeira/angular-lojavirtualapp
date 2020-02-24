00e338583961eb19aec41054e6cf82ab6b91f7d5
edward-teixeira <edwardteixeiradias@protonmail.com>
2020-02-24 16:52:38 -0300

    3 files modified, 3 added

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+  @Input() product; // Vai receber produto de ProductComponent
+  @Output() notify = new EventEmitter();
+
+  ngOnInit() {
+  }
+
+}
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -12,6 +12,10 @@ export class ProductListComponent {
 
   share() {
     window.alert('The product has been shared!');
+    console.log("Teste do botao");
+  }
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
 
